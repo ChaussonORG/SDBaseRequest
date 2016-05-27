ae927798ad762d5f472fecbdb9a4d8075481810e
chausson <232564026@qq.COM>
2016-05-27 19:14:23 +0800

    first commit

---
+++ SDBaseRequest/SDUpdateProfileAPI.h
@@ -0,0 +1,21 @@
+//
+//  SDUpdateProfileAPI.h
+//  FrameWorkPractice
+//
+//  Created by Chausson on 16/5/27.
+//  Copyright © 2016年 Chausson. All rights reserved.
+//
+
+#import <CHNetworking/CHNetworking.h>
+#import "SDBaseResponse.h"
+
+@interface SDUpdateProfileAPI : CHNetRequest
++ (instancetype)new __unavailable;
+- (instancetype)init  __unavailable;
+- (instancetype)initWithProfile:(NSDictionary *)profile;
+
+@property (nonatomic ,readonly ) SDBaseResponse *baseResponse;
+
+@property (nonatomic ,readonly ) NSDictionary *profile;
+
+@end
